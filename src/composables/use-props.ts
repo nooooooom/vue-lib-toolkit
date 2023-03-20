2e6205b6dbe4addc9f74041f9ecc0a6775a7f118
nooooooom <nooooooom@163.com>
2023-03-20 18:14:10 +0800

    fix(use-props): use set in Vue2

--- src/composables/use-props.ts
+++ src/composables/use-props.ts
@@ -6,38 +6,45 @@ import { isVue2 } from 'vue-module-demi'
 export function useProps<T>(currentInstance = getCurrentInstance()): T {
   const props = shallowReactive({})
 
-  const attrs = computed(() => {
-    const $attrs = currentInstance!.proxy?.$attrs
-    if (!$attrs) {
-      return {}
-    }
+  const updateProps = computed(() => {
     const attrs = {} as Record<string, any>
-    for (const attr of Object.keys($attrs)) {
-      // make a backup copy of attrs in props format
-      ;[camelize(attr), attr].forEach((key) => {
-        Object.defineProperty(attrs, key, {
-          enumerable: true,
-          get() {
-            return $attrs[attr]
-          }
-        })
-      })
+    const $attrs = currentInstance!.proxy?.$attrs
+    if ($attrs) {
+      for (const attr in $attrs) {
+        // make a backup copy of attrs in props format
+        attrs[camelize(attr)] = attrs[attr] = $attrs[attr]
+      }
     }
-    return attrs
+
+    return {
+      ...attrs,
+      ...(isVue2 ? (currentInstance!.proxy as any)?._props : currentInstance?.props)
+    }
   })
 
-  watchEffect(
-    () => {
-      Object.assign(
-        props,
-        attrs.value,
-        isVue2 ? (currentInstance!.proxy as any)?._props : currentInstance?.props
-      )
-    },
-    {
-      flush: 'pre'
-    }
-  )
+  if (isVue2) {
+    const set = (currentInstance?.proxy as any).$set
+    watchEffect(
+      () => {
+        const updatePropsValue = updateProps.value
+        for (const prop in updatePropsValue) {
+          set(props, prop, updatePropsValue[prop])
+        }
+      },
+      {
+        flush: 'pre'
+      }
+    )
+  } else {
+    watchEffect(
+      () => {
+        Object.assign(props, updateProps.value)
+      },
+      {
+        flush: 'pre'
+      }
+    )
+  }
 
   return props as T
 }
