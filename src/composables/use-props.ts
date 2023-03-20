a17eb003b4efbd2f8ac01a345f3771fa005b2d63
nooooooom <nooooooom@163.com>
2023-03-20 10:06:41 +0800

    chore(use-props): also merge attrs in vue3

--- src/composables/use-props.ts
+++ src/composables/use-props.ts
@@ -3,32 +3,34 @@ import { computed, getCurrentInstance, shallowReactive, watchEffect } from 'vue'
 import { isVue2 } from 'vue-module-demi'
 
 // Handle the situation that vue can only receive `props` by passing `options.props`
-export function useProps<T>(currentInstance = getCurrentInstance()) {
-  const props = shallowReactive({}) as T
+export function useProps<T>(currentInstance = getCurrentInstance()): T {
+  const props = shallowReactive({})
 
-  if (isVue2) {
-    const attrs = computed(() => {
-      const attrs = currentInstance!.proxy!.$attrs
-      if (!attrs) {
-        return {}
-      }
-      const asProps = { ...attrs } as Record<string, any>
-      for (const attr of Object.keys(attrs)) {
-        asProps[camelize(attr)] = attrs[attr]
-      }
-      return asProps
-    })
-    watchEffect(
-      () => Object.assign(props as any, attrs.value, (currentInstance!.proxy as any)?._props),
-      {
-        flush: 'pre'
-      }
-    )
-  } else {
-    watchEffect(() => Object.assign(props as any, currentInstance!.props), {
+  const attrs = computed(() => {
+    const attrs = currentInstance!.proxy?.$attrs
+    if (!attrs) {
+      return {}
+    }
+    // make a backup copy of attrs in props format
+    const asProps = {} as Record<string, any>
+    for (const attr of Object.keys(attrs)) {
+      asProps[camelize(attr)] = asProps[attr] = attrs[attr]
+    }
+    return asProps
+  })
+
+  watchEffect(
+    () => {
+      Object.assign(
+        props,
+        attrs.value,
+        isVue2 ? (currentInstance!.proxy as any)?._props : currentInstance?.props
+      )
+    },
+    {
       flush: 'pre'
-    })
-  }
+    }
+  )
 
-  return props
+  return props as T
 }
